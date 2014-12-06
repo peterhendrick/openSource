a5647097c1ea43b050385206e951f9fe96c3a3ba
peterhendrick <peterh@hdslabs.com>
2014-12-06 14:47:17 -0700

    proved out saucelabs open source and cleaned up messy files

--- test/conf.js
+++ test/conf.js
@@ -1,126 +1,18 @@
 exports.config = {
 
-  //To start a standalone Selenium Server locally
+  //seleniumServerJar: 'node_modules/.bin/webdriver-manager',
 
-  seleniumServerJar: null,
-  seleniumPort:null,
-  seleniumArgs: [],
-  chromeDriver: './selenium/chromedriver',
+  //seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
-  //To connect to a Selenium Server which is already running
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-
-  //To use remote browsers via Sauce Labs
-  sauceUser: null,
-  sauceKey: null,
+  sauceUser: 'peterjhendrick',
+  sauceKey: '472182a1-95b3-41e4-8068-e120321a1da9',
   sauceSeleniumAddress: null,
 
-  //To connect directly to Drivers
-
-  directConnect: false,
-  firefoxPath: null,
-  chromeOnly: false,
-
-  //What tests to run
-
-  // Spec patterns are relative to this conf.js file
   specs: [
-    'spec/*_spec.js',
-  ]
-
-  // Patterns to exclude
-  exclude: [],
-
-
-  suites: {
-    smoke: 'spec/smoketests/*.js',
-    full: 'spec/*.js'
-  },
-
-  // How to set up browsers, for more info on capabilities see https://code.google.com/p/selenium/wiki/DesiredCapabilities
-  // You can also specify count, shardTestFiles, and maxInstances.
-
-  capabilities: {
-    browserName: 'chrome',
-    count: 1,
-    shardTestFiles: false,
-    maxInstances: 1,
-    specs: ['spec/chromOnlySpec.js']
-    exclude: ['spec/doNotRunInChromeSpec.js']
-  },
-
-  multiCapabilities: [{
-    'browserName': 'chrome',
-    'count': 1,
-    'shardTestFiles': false,
-    'maxInstances': 1,
-    } , {
-    'browserName': 'firefox',
-    'count': 2,
-    'shardTestFiles': false,
-    'maxInstances': 1,
-    }
+    'spec.js'
   ],
 
-  //Maximum number of total browser sessions to run at the sme time
-
-  maxSessions: -1,
-
-
-  // ---------------------------------------------------------------------------
-  // ----- Global test information ---------------------------------------------
-  // ---------------------------------------------------------------------------
-
-  baseUrl: 'http://localhost:5000',
-
-  //CSS selector for the element housing the angular app
-  rootElement: 'body',
-
-  // Timeout of browser scripts
-  allScriptsTimeout: 11000,
-
-  //Page load timeout
-  getPageTimeout: 10000,
-
-  beforeLaunch: function() {
+  capabilities: {
+    'browserName': 'chrome'
   },
-
-  onPrepare: function() {
-  },
-
-  onComplete: function() {
-  },
-
-  onCleanUp: function(exitcode) {
-  },
-
-  afterLaunch: function() {
-  },
-
-  params: {
-    login: {
-      user: 'user',
-      password: 'pass',
-    }
-  },
-
-  framework: 'jasmine',
-
-  jasmineNodeOpts: {
-    isVerbose: false,
-    showColors: true,
-    includeStackTrace: true,
-    defaultTimeoutInterval: 30000
-  },
-
-  mochaOpts: {
-    ui: 'bdd',
-    reporter: 'list'
-  },
-
-  cucumberOpts: {
-    require: 'cucumber/stepDefinitions.js',
-    tags: '@dev',
-    format: 'summary'
-  }
-}
+};
--- test/spec.js
+++ test/spec.js
@@ -2,13 +2,16 @@ describe('user', function (){
   beforeEach(function() {
     browser.get('http://localhost:3000')
   });
+  afterEach(function() {
+
+  });
 
   it('should be able to sign up and recieve a message', function () {
     element(by.id('signUp')).click();
-    element(by.id('firstName')).sendKeys('Peter');
-    element(by.id('lastName')).sendKeys('Hendrick');
-    element(by.id('email')).sendKeys('jowdaddsafda@la.com');
-    element(by.id('username')).sendKeys('chdifdfaenveseman');
+    element(by.id('firstName')).sendKeys('user');
+    element(by.id('lastName')).sendKeys('name');
+    element(by.id('email')).sendKeys('usera@name.com');
+    element(by.id('username')).sendKeys('username');
     element(by.id('password')).sendKeys('pass');
 
     element(by.id('submitButton')).click();
