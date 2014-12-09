56b079b32b8ec24fe4476ee8a245a592562acd04
peterhendrick <peterh@hdslabs.com>
2014-12-09 10:52:33 -0700

    modified selenium server jar

--- test/conf.js
+++ test/conf.js
@@ -1,12 +1,13 @@
 exports.config = {
 
-  //seleniumServerJar: 'C:/Users/Peter/mostesting/mean/node_modules/protractor/bin/webdriver-manager',
-
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  seleniumServerJar: 'C:/Users/Peter/openSource/node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar',
+  
+  //chromeDriver: 'C:/Users/Peter/openSource/node_modules/protractor/selenium/chromedriver',
+  //seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
   // sauceUser: 'peterjhendrick',
   // sauceKey: '472182a1-95b3-41e4-8068-e120321a1da9',
-  //sauceSeleniumAddress: null,
+  // sauceSeleniumAddress: null,
 
   specs: [
     'spec.js'
