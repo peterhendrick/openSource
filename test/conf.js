074aaf7bde80cae08e9353ce75d2a5af9658ed46
Peter Hendrick <peterh@hdslabs.com>
2015-01-06 16:23:35 -0700

    started selenium on testing

--- test/conf.js
+++ test/conf.js
@@ -1,15 +1,15 @@
 exports.config = {
 
-  //seleniumServerJar: 'node_modules/.bin/webdriver-manager',
+  seleniumServerJar: '/home/peter/openSource/node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar',
 
   //seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
-  sauceUser: 'peterjhendrick',
-  sauceKey: '472182a1-95b3-41e4-8068-e120321a1da9',
-  sauceSeleniumAddress: null,
+  // sauceUser: 'peterjhendrick',
+  // sauceKey: '472182a1-95b3-41e4-8068-e120321a1da9',
+  // sauceSeleniumAddress: null,
 
   specs: [
-    'spec.js'
+    './spec.js'
   ],
 
   capabilities: {
